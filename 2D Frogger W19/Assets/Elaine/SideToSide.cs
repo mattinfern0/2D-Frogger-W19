64652ee3c661a2c485d8adbf9194b4eae3a5716b
elaine-ho <elaine48e@gmail.com>
2019-03-11 14:44:24 -0700

    Log/Car Things

---
+++ 2D Frogger W19/Assets/Elaine/SideToSide.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideToSide : MonoBehaviour
+{
+    public float pos_x_start;
+    public float pos_x_end;
+    public float speed = 1f;
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float width = GetComponent<BoxCollider2D>().bounds.size.x;
+        if (pos_x_start>pos_x_end){
+            transform.position += transform.right*-speed*Time.deltaTime;
+            if(transform.position.x+width<pos_x_end){
+                Vector3 temp = new Vector3(pos_x_start + width,transform.position.y,transform.position.z);
+                transform.position = temp;
+            }
+        }
+        else{
+            transform.position += transform.right*speed*Time.deltaTime;
+            if(transform.position.x+width>pos_x_end){
+                Vector3 temp = new Vector3(pos_x_start-width,transform.position.y,transform.position.z);
+                transform.position=temp;
+            }
+        }
+    }
+}
